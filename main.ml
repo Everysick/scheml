31b8f30aa79039b88146e1eb4dd7aa1d143b3049
everysick <s.wakeup31@gmail.com>
2018-09-09 18:14:43 +0900

    Add callcc

--- main.ml
+++ main.ml
@@ -1,4 +1,5 @@
 type ident = string
+module IdentEnv = Map.Make(String)
 
 module Sx = struct
   type t =
@@ -6,77 +7,93 @@ module Sx = struct
     | Cdr of t
     | Cons of t * t
     | App of t * t
+    | Callcc of t
     | Lambda of ident * t
     | Define of ident * t
     | Var of ident
     | Symbol of string
-    | Pair of t * t (* Unavailable in User input *)
-end
-
-module IdentEnv = Map.Make(String)
-
-module Env = struct
-  type t = Sx.t IdentEnv.t
-  let empty : t = IdentEnv.empty
+    | LambdaVal of ident * env * t (* Unavailable in User input *)
+    | ContVal of env * (t * env -> t * env) (* Unavailable in User input *)
+    | PairVal of t * t (* Unavailable in User input *)
+   and env = t IdentEnv.t
 end
 
 module Evaluator = struct
-  let rec eval (env : Env.t) (s_x : Sx.t) (cont : Sx.t * Env.t -> Sx.t * Env.t) : Sx.t * Env.t =
+  let rec eval (env : Sx.t IdentEnv.t) (s_x : Sx.t) (cont : Sx.t * Sx.t IdentEnv.t -> Sx.t * Sx.t IdentEnv.t ) : Sx.t * Sx.t IdentEnv.t =
+    let merge_env env env' =
+      IdentEnv.merge (fun k xo yo -> match xo, yo with
+                                     | Some x, Some y -> xo
+                                     | None, y -> yo
+                                     | x, None -> xo
+                     ) env env' in
     match s_x with
     | Sx.Car(cons) ->
        eval env cons (fun (pair, _) ->
               match pair with
-              | Sx.Pair(e, _) -> cont (e, env)
+              | Sx.PairVal(e, _) -> cont (e, env)
               | _ -> failwith "Unmatch type with Sx.Pair in Car")
     | Sx.Cdr(cons) ->
        eval env cons (fun (pair, _) ->
               match pair with
-              | Sx.Pair(_, e) -> cont (e, env)
+              | Sx.PairVal(_, e) -> cont (e, env)
               | _ -> failwith "Unmatch type with Sx.Pair in Car")
     | Sx.Cons(e1, e2) ->
        eval env e1 (fun (e1', _) ->
               eval env e2 (fun (e2', _) ->
-                     cont (Sx.Pair(e1', e2'), env)))
+                     cont (Sx.PairVal(e1', e2'), env)))
     | Sx.App(e, arg) ->
        eval env e (fun (e', _) ->
               eval env arg (fun (arg', _) ->
                      (match e' with
-                      | Sx.Lambda(x, body) ->
-                         let env' = IdentEnv.add x arg' env in
-                         eval env' body cont
-                      | _ -> failwith "Unmatch type with Sx.Lambda")))
-    | Sx.Lambda(x, body) -> cont (s_x, env)
+                      | Sx.LambdaVal(x, env', body) ->
+                         let env' = IdentEnv.add x arg' env' in
+                         eval (merge_env env env') body cont
+                      | Sx.ContVal(env', body) ->
+                         body (arg', (merge_env env env'))
+                      | _ -> failwith "Unmatch type with Sx.LambdaVal or Sx.ContVal in App")))
+    | Sx.Callcc(e) ->
+       eval env e (fun (e', _) ->
+              (match e' with
+               | Sx.LambdaVal(x, env', body) ->
+                  let env' = IdentEnv.add x (Sx.ContVal(env, cont)) env in
+                  eval env' body cont
+               | _ -> failwith "Unmatch type with Sx.Lambda in Callcc"))
+    | Sx.Lambda(x, body) -> cont (Sx.LambdaVal(x, env, body), env)
     | Sx.Define(x, v) ->
-       let env' = IdentEnv.add x v env in
-       cont (Sx.Var(x), env')
+       eval env v (fun (v', _) ->
+              let env' = IdentEnv.add x v' env in
+              cont (Sx.Var(x), env'))
     | Sx.Var(x) -> cont (IdentEnv.find x env, env)
     | Sx.Symbol(str) -> cont (s_x, env)
-    | Sx.Pair(v1, v2) -> cont (s_x, env)
+    | _ -> failwith "Unexpected expression by User Program"
 end
 
 module Test = struct
   let test (program : Sx.t list) : Sx.t list =
-    let rec walk (env: Env.t) (program : Sx.t list) : Sx.t list =
+    let rec walk (env: Sx.t IdentEnv.t) (program : Sx.t list) : Sx.t list =
       match program with
       | hd :: tl ->
          let (s_x, env') = Evaluator.eval env hd (fun x -> x) in
          (s_x) :: (walk env' tl)
       | [] -> []
     in
-    walk Env.empty program
+    walk IdentEnv.empty program
 end
 
 let program =
   [
     Sx.Car(Sx.Cons(Sx.Symbol("hello"), Sx.Symbol("world")));
     Sx.Cdr(Sx.Cons(Sx.Symbol("hello"), Sx.Symbol("world")));
-    Sx.Define("x", Sx.Lambda("y", Sx.Var("y")));
-    Sx.App(Sx.Var("x"), Sx.Cdr(Sx.Cons(Sx.Symbol("hello"), Sx.Symbol("world"))));
+    Sx.Define("x", Sx.Lambda("y", Sx.Cdr(Sx.Var("y"))));
+    Sx.App(Sx.Var("x"), Sx.Cons(Sx.Symbol("hello"), Sx.Symbol("world")));
   ]
 
-let result = Test.test program
+let callcc_program =
+  [
+    Sx.Cons(Sx.Callcc(Sx.Lambda("c", Sx.Symbol("hello"))), Sx.Symbol("world"));
+    Sx.Cons(Sx.Callcc(Sx.Lambda("c", Sx.Cons(Sx.Symbol("hello"), Sx.App(Sx.Var("c"), Sx.Symbol("HELLO"))))), Sx.Symbol("world"));
+  ]
 
-(*
 let loop_program =
   [
     Sx.Define("x", Sx.Lambda("g", Sx.App(Sx.Var("y"), Sx.Var("g"))));
@@ -84,5 +101,6 @@ let loop_program =
     Sx.App(Sx.Var("x"), Sx.Symbol("loop"));
   ]
 
-let result = Test.test loop_program
-*)
+let result = Test.test program
+let loop_result = Test.test loop_program
+let callcc_result = Test.test callcc_program
